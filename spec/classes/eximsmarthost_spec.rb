f7e36ca6b817956e80728d406d20d8d701dc2572
Dan Foster <dan@zem.org.uk>
2014-11-23 13:21:56 +0000

    Add more simple tests

--- spec/classes/eximsmarthost_spec.rb
+++ spec/classes/eximsmarthost_spec.rb
@@ -2,13 +2,18 @@ require 'spec_helper'
 
 describe 'eximsmarthost' do
   it {
+    should contain_package('postfix').with({
+      'ensure' => 'absent',
+    })
     should contain_package('exim').with({
       'ensure' => 'present',
     })
-  }
-  it {
     should contain_service('exim').with({
       'ensure' => 'running',
     })
+    should contain_file('/etc/exim/exim.conf').with({
+      'ensure' => 'present',
+    })
+    should contain_file('/etc/aliases')
   }
 end
